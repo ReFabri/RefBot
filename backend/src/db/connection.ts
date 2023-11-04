f51eeb9d8e63c635ea77e7b1d19d42477ae69c86
Renato <renato.f.fabri@gmail.com>
2023-11-03 23:51:37 -0300

    ADD MongoDB connection

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose";
+
+async function connectToDatabase() {
+  try {
+    await connect(process.env.MONGO_URI);
+  } catch (error) {
+    console.error(error);
+    throw new Error("Cannot connect to MongoDB");
+  }
+}
+
+async function disconnectFromDatabase() {
+  try {
+    await disconnect();
+  } catch (error) {
+    console.error(error);
+    throw new Error("Cannot disconnect from MongoDB");
+  }
+}
+
+export { connectToDatabase, disconnectFromDatabase };
