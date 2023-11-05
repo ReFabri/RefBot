7e0c88373cdb55ea13278b1c7c7cc5e71dd1de62
Renato <renato.f.fabri@gmail.com>
2023-11-05 19:02:35 -0300

    Add Morgan, add index route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { config } from "dotenv";
+import { morgan } from "morgan";
+
 config();
 
 const app = express();
 
 app.use(express.json());
+
+// Remove morgan in production
+app.use(morgan("dev"));
+
+app.use("/api/v1");
 
 export default app;
