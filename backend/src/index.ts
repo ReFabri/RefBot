21feef5fb4ecb18736b9169f437607b472a9fd81
Renato <renato.f.fabri@gmail.com>
2023-11-05 18:37:00 -0300

    index.ts cleanup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,14 @@
-import express from "express";
-import { config } from "dotenv";
-config();
+import app from "./app.js";
+import { connectToDatabase } from "./db/connection.js";
 
-const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
-
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000`);
-});
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running and connected to database`);
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed", error);
+  });
