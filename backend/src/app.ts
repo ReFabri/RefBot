a2258045abb2907b4ffd40ac1c60ec492c169e9b
Renato <renato.f.fabri@gmail.com>
2023-11-05 21:53:37 -0300

    Add GetAllUsers controller

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import { morgan } from "morgan";
+import morgan from "morgan";
 import appRouter from "./routes/index.js";
 
 config();
--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -2,7 +2,7 @@ import { connect, disconnect } from "mongoose";
 
 async function connectToDatabase() {
   try {
-    await connect(process.env.MONGO_URI);
+    await connect(process.env.MONGODB_URI);
   } catch (error) {
     console.error(error);
     throw new Error("Cannot connect to MongoDB");
