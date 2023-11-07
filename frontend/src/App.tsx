25a8f632f44610adc2408804d0101e24f069cff2
Renato <renato.f.fabri@gmail.com>
2023-11-07 15:42:28 -0300

    Add all pages routes to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Signup from "./pages/Signup";
+
 function App() {
-  return <main>RefBot</main>;
+  return (
+    <main>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/notfound" element={<NotFound />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
