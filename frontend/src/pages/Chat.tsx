95e6d7afe653d0e2dee926de08325134b691a6d9
Renato <renato.f.fabri@gmail.com>
2023-11-09 19:28:51 -0300

    ADD start frontend Chat page

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { Box, Avatar } from "@mui/material";
+import { useAuth } from "../context/AuthContext";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  const auth = useAuth();
+  const avatarName = auth?.user?.name
+    .split(" ")
+    .filter((str, i) => i <= 1)
+    .map((str) => str[0])
+    .join("");
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        mt: 3,
+        gap: 3,
+      }}
+    >
+      <Box
+        sx={{
+          display: { md: "flex", xs: "none", sm: "none" },
+          flex: 0.2,
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            width: "100%",
+            height: "60vh",
+            bgcolor: "rgb(17, 29, 39)",
+            borderRadius: 5,
+            flexDirection: "column",
+            mx: 3,
+          }}
+        >
+          <Avatar
+            sx={{
+              mx: "auto",
+              my: 2,
+              bgcolor: "white",
+              color: "black",
+              fontWeight: 700,
+            }}
+          >
+            {avatarName}
+          </Avatar>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Chat;
