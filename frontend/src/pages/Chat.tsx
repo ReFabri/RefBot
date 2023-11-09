edaf928508f2e4140ac0158a426952b23f51819a
Renato <renato.f.fabri@gmail.com>
2023-11-09 20:28:23 -0300

    ADD ChatItem component

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,5 +1,6 @@
-import { Box, Avatar } from "@mui/material";
+import { Box, Avatar, Typography, Button } from "@mui/material";
 import { useAuth } from "../context/AuthContext";
+import red from "@mui/material/colors/red";
 
 const Chat = () => {
   const auth = useAuth();
@@ -8,6 +9,15 @@ const Chat = () => {
     .filter((str, i) => i <= 1)
     .map((str) => str[0])
     .join("");
+  const chatMessages = [
+    { role: "user", content: "Hello, how are you?" },
+    { role: "assistant", content: "I am fine, how are you today?" },
+    { role: "user", content: "I am fine too, thank you for asking!" },
+    {
+      role: "assistant",
+      content: "It was my pleasure! I am glad you are feeling good today.",
+    },
+  ];
 
   return (
     <Box
@@ -32,7 +42,7 @@ const Chat = () => {
             display: "flex",
             width: "100%",
             height: "60vh",
-            bgcolor: "rgb(17, 29, 39)",
+            bgcolor: "rgb(17,29,39)",
             borderRadius: 5,
             flexDirection: "column",
             mx: 3,
@@ -49,6 +59,67 @@ const Chat = () => {
           >
             {avatarName}
           </Avatar>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+            You are talking to a ChatBOT
+          </Typography>
+          <Typography sx={{ mx: "auto", fontFamily: "work sans", my: 4, p: 3 }}>
+            You can ask some questions related to Knowledge, Business, Advices,
+            Education, etc. But avoid sharing personal information
+          </Typography>
+          <Button
+            sx={{
+              width: "200px",
+              my: "auto",
+              color: "white",
+              fontWeight: "700",
+              borderRadius: 3,
+              mx: "auto",
+              bgcolor: red[300],
+              ":hover": {
+                bgcolor: red.A400,
+              },
+            }}
+          >
+            Clear Conversation
+          </Button>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flex: { md: 0.8, xs: 1, sm: 1 },
+          flexDirection: "column",
+          px: 3,
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "40px",
+            color: "white",
+            mb: 2,
+            mx: "auto",
+            fontWeight: "600",
+          }}
+        >
+          Model - GPT 3.5 Turbo
+        </Typography>
+        <Box
+          sx={{
+            width: "100%",
+            height: "60vh",
+            borderRadius: 3,
+            mx: "auto",
+            display: "flex",
+            flexDirection: "column",
+            overflow: "scroll",
+            overflowX: "hidden",
+            overflowY: "auto",
+            scrollBehavior: "smooth",
+          }}
+        >
+          {chatMessages.map((chat) => (
+            <div>{chat.content}</div>
+          ))}
         </Box>
       </Box>
     </Box>
