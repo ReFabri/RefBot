5f1a4c2fbac017d59d47e7608a2ff4f2daddd8bf
Renato <renato.f.fabri@gmail.com>
2023-11-09 15:09:40 -0300

    ADD initial openai API implementation

--- backend/src/routes/chatRoutes.ts
+++ backend/src/routes/chatRoutes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
+import { verifyToken } from "../utils/tokenManager.js";
 
 const chatRoutes = Router();
+chatRoutes.post("/new", verifyToken);
 
 export default chatRoutes;
