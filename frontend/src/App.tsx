e8ae5192bcf5bcf44508fc2b1c9ba42bcc4e85af
Renato <renato.f.fabri@gmail.com>
2023-11-10 17:47:42 -0300

    Fix Protect /chat route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,22 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import Signup from "./pages/Signup";
+import { useAuth } from "./context/AuthContext";
 
 function App() {
+  const auth = useAuth();
+
   return (
     <main>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/chat" element={<Chat />} />
         <Route path="/login" element={<Login />} />
         <Route path="/notfound" element={<NotFound />} />
         <Route path="/signup" element={<Signup />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
       </Routes>
     </main>
   );
